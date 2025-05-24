fe664df3cf4023acf7b137f5f37d9ceb1ede10b3
tj-ftm <159525990+tj-ftm@users.noreply.github.com>
2025-05-24 22:05:34 +0530

    s

--- src/game/ImageManager.ts
+++ src/game/ImageManager.ts
@@ -9,20 +9,20 @@ export class ImageManager {
   private imageConfig: ImageConfig = {
     playerIdle: {
       src: '/assets/Icons/playeridle.gif',
-      width: 700,
-      height: 900,
+      width: 70,
+      height: 90,
       frames: 4
     },
     playerRun: {
       src: '/assets/Icons/playersprite.gif',
-      width: 700,
-      height: 900,
+      width: 70,
+      height: 90,
       frames: 4
     },
     playerJump: {
       src: '/assets/Icons/playersprite.gif',
-      width: 700,
-      height: 900,
+      width: 70,
+      height: 90,
       frames: 4
     },
     background: {
